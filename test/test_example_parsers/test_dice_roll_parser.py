b84d7b21e7062d2c430e44adda5bde20458331c3
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-02-23 13:02:45 -0600

    Asdd error messages for asserts.

--- test/test_example_parsers/test_dice_roll_parser.py
+++ test/test_example_parsers/test_dice_roll_parser.py
@@ -10,18 +10,19 @@ def die():
 
 class TestSingleRoll:
     def test_single_roll_max_one(self, die):
-        assert die.evaluate('d1') == 1
+        assert die.evaluate('d1') == 1, "Single roll of one sided die failed to land on 1."
 
     @pytest.mark.parametrize('die_max',
                              [2, 4, 5, 10, 100, ],
                              )
     def test_single_roll_ranges(self, die,
                                 die_max):
-        assert 1 <= die.evaluate(f'd{die_max}') <= die_max
+        assert 1 <= die.evaluate(f'd{die_max}') <= die_max, \
+            "Die should return value within range of number of sides inclusive."
 
     def test_single_roll_error_empty_range(self, die):
         with pytest.raises(ValueError):
-            die.evaluate('d0')
+            die.evaluate('d0'), "Rolling die with zero sides should fail."
 
 
 class TestMultipleRolls:
@@ -30,14 +31,15 @@ class TestMultipleRolls:
                              )
     def test_zero_rolls_returns_zero(self, die,
                                      die_max):
-        assert die.evaluate(f'0d{die_max}') == 0
+        assert die.evaluate(f'0d{die_max}') == 0, "Rolling zero times should fail."
 
     @pytest.mark.parametrize('num_rolls',
                              [1, 2, 4, 5, 6, 10, 100, ],
                              )
     def test_multiple_rolls_max_one(self, die,
                                     num_rolls):
-        assert die.evaluate(f'{num_rolls}d1') == num_rolls
+        assert die.evaluate(f'{num_rolls}d1') == num_rolls, \
+            "Incorrect evaluation of multiple die rolls: each roll should equal 1."
 
     @pytest.mark.parametrize('num_rolls',
                              [1, 2, 4, 5, 10, 100, ],
@@ -47,7 +49,8 @@ class TestMultipleRolls:
                              )
     def test_single_roll_ranges(self, die,
                                 num_rolls, die_max):
-        assert num_rolls <= die.evaluate(f'{num_rolls}d{die_max}') <= num_rolls*die_max
+        assert num_rolls <= die.evaluate(f'{num_rolls}d{die_max}') <= num_rolls*die_max, \
+            "Incorrect evaluation of multiple die rolls."
 
 
 class TestCompoundExamples:
@@ -62,8 +65,10 @@ class TestCompoundExamples:
     def test_compound_examples(self, die,
                                evaluation_string,
                                result_min, result_max):
-        assert result_min <= die.evaluate(evaluation_string) <= result_max
+        assert result_min <= die.evaluate(evaluation_string) <= result_max, \
+            "Incorrect evaluation of multiple die rolls."
 
     def test_rolls_are_distinct(self, die):
         pairs_of_rolls = [die.evaluate('2d6') for _ in range(100)]
-        assert any(pair%2 != 0 for pair in pairs_of_rolls)
+        assert any(pair%2 != 0 for pair in pairs_of_rolls), \
+            "Incorrect evaluation of multiple die rolls: no odd results in 100 double rolls."
