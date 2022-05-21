d2ffef9524f3e9646be388ba13a234e033e1f107
ptmcg <ptmcg@austin.rr.com>
2022-05-21 08:13:05 -0500

    Added min, max, and show functions to DiceRollParser

--- plusminus/examples/dice_roll_parser.py
+++ plusminus/examples/dice_roll_parser.py
@@ -6,6 +6,7 @@
 from plusminus import BaseArithmeticParser
 
 
+# fmt: off
 class DiceRollParser(BaseArithmeticParser):
     """
     Parser for evaluating expressions representing rolls of dice, as used in many board and
@@ -14,16 +15,22 @@ class DiceRollParser(BaseArithmeticParser):
         d20
         3d20
         5d6 + d20
+        min(d6, d6, d6)
+        show(d6, d6, d6)
     """
 
     def customize(self):
         import random
 
-        # fmt: off
         self.add_operator("d", 1, BaseArithmeticParser.RIGHT, lambda a: random.randint(1, a))
         self.add_operator("d", 2, BaseArithmeticParser.LEFT,
                           lambda a, b: sum(random.randint(1, b) for _ in range(a)))
-        # fmt: on
+
+        self.add_function("min", ..., min)
+        self.add_function("max", ..., max)
+        self.add_function("show", ..., lambda *args: (list(args), sum(args)))
+
+# fmt: on
 
 
 if __name__ == '__main__':
@@ -35,6 +42,8 @@ if __name__ == '__main__':
         3d6
         d20+3d4
         2d100
+        max(d6, d6, d6)
+        show(d6, d6, d6)        
         """,
         postParse=lambda _, result: result[0].evaluate(),
     )
